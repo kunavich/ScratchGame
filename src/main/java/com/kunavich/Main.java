97bf91252e19e14f92772d7ccdb83491afe34181
kunavich <Kunpash@gmail.com>
2024-10-15 20:23:33 +0200

    added jackson

--- src/main/java/com/kunavich/Main.java
+++ src/main/java/com/kunavich/Main.java
@@ -1,10 +1,16 @@
 package com.kunavich;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kunavich.entity.Config;
+
+import java.io.File;
 
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Matrix matrix= new Matrix();
+        Config config = objectMapper.readValue(new File("config.json"), Config.class);
     }
 
 }
